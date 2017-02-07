d3e205a416fc815459e750f991deb1638ecc7e57
PoojanParikh <camkampar@gmail.com>
2017-02-07 12:30:09 +0530

    Single row list view xml Created for JSON Parsing with List View

--- app/src/main/java/com/poojan/myapplication/CustomAdapter.java
+++ app/src/main/java/com/poojan/myapplication/CustomAdapter.java
@@ -1,31 +1,56 @@
 package com.poojan.myapplication;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by VNurtureTechnologies on 07/02/17.
  */
 
 public class CustomAdapter extends BaseAdapter {
+
+    Context context;
+    LayoutInflater inflater;
+    ArrayList<Post> posts;
+
+    CustomAdapter(Context context, ArrayList<Post> posts,LayoutInflater inflater){
+
+        this.context=context;
+        this.posts=posts;
+        this.inflater = inflater;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return posts.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return i;
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
+    }
+
+    static class ViewHolder{
+
+
+
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+
+
+
+        return view;
     }
 }
